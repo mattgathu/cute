3ac6267e1b71b2dbd9e1839b6d1324f944d63d24
Matt Gathu <mattgathu@gmail.com>
2017-07-16 08:08:31 +0300

    fixed typo

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,11 @@
 //! allowing for conditionals and nested comprehensions.
 //!
 //! # Python Syntax
-//! ``
+//! ```
 //! squares = [x*x for x in range(10)]
 //!
 //! even_squares = [x*x for x in range(10) if x % 2 == 0]
-//! ``
+//! ```
 //!
 //! # c! Syntax
 //! ```
