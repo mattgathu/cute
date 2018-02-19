d1f26a3aab11201e88753e0acaa61f9270bfdc23
Travis CI User <travis@example.org>
2018-02-19 14:21:42 +0000

    Update documentation

---
+++ search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex["cute"] = {"doc":"A Macro for python-esque list and dictionary(hashmap) comprehensions in Rust","items":[[14,"c","cute","",null,null]],"paths":[]};
+initSearch(searchIndex);
